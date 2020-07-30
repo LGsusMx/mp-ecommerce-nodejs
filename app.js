09f00d07faf55afeabfc91b26ab308f3739550fc
LGsus Rmz <caednicolas2@gmail.com>
2020-07-30 01:10:12 -0500

    what

--- app.js
+++ app.js
@@ -1,161 +1,89 @@
-var dotenv = require('dotenv');
-var express = require("express");
-var exphbs = require("express-handlebars");
-const mercadopago = require("mercadopago");
-const fetch = require('node-fetch');
+require('./config/config');
+var express = require('express');
+var exphbs = require('express-handlebars');
+const mercadopago = require('mercadopago');
+const bodyParser = require('body-parser');
+const https = require('https');
 
 var app = express();
-global.asd = 'Hola'
 
-dotenv.config();
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
-mercadopago.configure({
-  access_token: process.env.ACCESS_TOKEN
+// parse application/json
+app.use(bodyParser.json())
+
+// configuracion de handlebars
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// configuracion de rutas
+app.get('/', function(req, res) {
+    res.render('home');
 });
-
-app.engine("handlebars", exphbs());
-app.set("view engine", "handlebars");
-
-app.get("/", function(req, res) {
-  res.render("home");
+app.get('/detail', function(req, res) {
+    res.render('detail', req.query);
 });
-
-app.get("/detail", function(req, res) {
-  const { title, price, unit, img } = req.query;
-  let preference = {
-    items: [
-        {
-            id : "1234",
-            title: title,
-            descrption: "Dispositivo móvil de Tienda e-commerce",
-            picture_url: img,
-            unit_price: parseFloat(price),
-            quantity: parseInt(unit)
-        }
-    ],
-    back_urls: {
-        success: req.protocol + '://' + req.get('host') + '/success',
-        pending: req.protocol + '://' + req.get('host') + '/pending',
-        failure: req.protocol + '://' + req.get('host') + '/failure'
-    },
-    payer: {
-        name: "Lalo",
-        surname: "Landa",
-        identification: {
-            type : "DNI",
-            number : "535650015"
-        },
-        email : "test_user_58295862@testuser.com",
-        phone: { area_code: "52", number: 5549737300 },
-        address: {
-            street_name: "Insurgentes Sur",
-            street_number: 1602,
-            zip_code: "0394​0"
-        }
-    },
-    payment_methods: {
-        excluded_payment_methods: [
-          { id: "amex" }
-        ],
-        excluded_payment_types: [
-          { id: "atm" }
-        ],
-        installments: 6
-     },
-    external_reference: "caednicolas2@gmail.com",
-    notification_url: req.protocol + '://' + req.get('host') + '/notifications',
-    auto_return:"approved"
-  };
-
-  mercadopago.preferences.create(preference)
-    .then(resp => {
-      global.init_point = resp.body.init_point;
-      console.log(global.init_point);
-      global.asd = resp.body.id;
-      res.render("detail", {
-        img,
-        price,
-        title,
-        unit,
-        id: resp.body.id
-      });
-    })
-    .catch(err => console.log(err));
+app.get('/rejected', function(req, res) {
+    res.render('rejected');
 });
+app.get('/pending', function(req, res) {
+    res.render('pending');
+});
+app.get('/approved', function(req, res) {
+    const url = 'https://api.mercadopago.com/v1/payments/' + req.query.payment_id + '?access_token=' + process.env.ACCESS_TOKEN_PROD;
+    https.get(url, resp => {
+            let data = '';
 
-app.post('/procesar-pago', (req, res) => {
-
-  fetch('https://api.mercadopago.com//merchant_orders/:id?access_token=ACCESS_TOKEN_ENV'+process.env.ACCESS_TOKEN)
-    .then(res => res.json())
-    .then(json => {
-
-      let status = json.elements[0].payments[0].status;
-      console.log(status);
-      switch(status) {
-        case 'pending':
-          res.render("pending");
-          break;
-        case 'approved':
-          var id = json.elements[0].payments[0].id
-          mercadopago.payment.get(id).then(function (data) {
-            var payment_method_id = data.body.payment_method_id
-            var transaction_amount = data.body.transaction_amount
-            var order_id = data.body.order.id
-            res.render('success', {
-              id,
-              payment_method_id,
-              transaction_amount,
-              order_id
+            resp.on('data', (chunk) => {
+                data += chunk;
             });
-          })
-          break;
-        case 'failure':
-          res.render("failure");
-          break;
-        default:
-          res.status(500).json('Error-Guardado')
-      }
-    });
-})
+            resp.on('end', () => {
+                res.render('approved', JSON.parse(data));
+            });
+        })
+        .on("error", (err) => {
+            console.log("Error: " + err.message);
+        });
+});
+app.get('/checkout/payment', (req, res) => {
+    if (req.query.payment_status === 'approved') {
+        const url = 'https://api.mercadopago.com/v1/payments/' + req.query.payment_id + '?access_token=' + process.env.ACCESS_TOKEN_PROD;
+        https.get(url, resp => {
+                let data = '';
 
-app.get('/', function (req, res) {
-  res.render('home');
+                resp.on('data', (chunk) => {
+                    data += chunk;
+                });
+                resp.on('end', () => {
+                    res.render('approved', JSON.parse(data));
+                });
+            })
+            .on("error", (err) => {
+                console.log("Error: " + err.message);
+            });
+    } else {
+        if (req.query.payment_status === 'pending' || req.query.payment_status === 'in_process') {
+            res.render('pending');
+        } else {
+            res.render('rejected');
+        }
+    }
 });
 
-app.get('/failure', function (req, res) {
-  res.render('failure');
+app.post('/checkout/webhook', (req, res) => {
+    console.log("hay retorno");
+    console.log(req);
+    res.json('Aca se reciben notificaciones');
 });
 
-app.get('/pending', function (req, res) {
-  res.render('pending');
+// configuración de mercadopago
+mercadopago.configure({
+    access_token: process.env.ACCESS_TOKEN_PROD
 });
 
-app.get('/success', function (req, res) {
-    var id = req.query.collection_id
-    mercadopago.payment.get(id).then(function (data) {
-      var payment_method_id = data.body.payment_method_id
-      var transaction_amount = data.body.transaction_amount
-      var order_id = data.body.order.id
-      res.render('success', {
-        id,
-        payment_method_id,
-        transaction_amount,
-        order_id
-      });
-    })
+app.use(express.static('assets'));
 
-});
+app.use('/assets', express.static(__dirname + '/assets'));
 
-app.post('/notifications', async(req, res)=>{
-  
-    const body = req.body;
-    console.log("Maldo-Data-notifications")
-    console.log(req.query);
-    res.status(200).json('OK')
-})
-
-app.use(express.static("assets"));
-
-app.use("/assets", express.static(__dirname + "/assets"));
-
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT);
